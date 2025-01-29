b95f1dd7f889ad7d09bc2863c1503ff5fe9b70d1
NITESH YADAV <nitesh.yadav2009@gmail.com>
2025-01-29 14:06:02 +0530

    feat(App.js) text update

--- 04-using-actions/react-app/src/App.js
+++ 04-using-actions/react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Nitesh
+          Learn React with Nitesh Yadav
         </a>
       </header>
     </div>
